9485d0d9a10cbe6c56288762c61bd7bc7aac92a1
Britton Ballard <davidbritton.ballard@gmail.com>
2011-03-18 21:29:56 -0500

    Got Rspec up and running...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
+Dir.foreach(File.dirname(__FILE__) + "/../app/") do |f| 
+  require File.dirname(__FILE__) + "/../app/" + f if f =~ /\w+\.rb/
+end
+
 RSpec.configure do |config|
   config.mock_with :rspec
 end
