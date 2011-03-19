c57b26ad3a47e0f0606e34d24ab04650fa5d58d9
Britton Ballard <davidbritton.ballard@gmail.com>
2011-03-19 00:03:08 -0500

    Build, build, build...

--- app/product.rb
+++ app/product.rb
@@ -1,10 +1,22 @@
 class Product
-  attr_accessor :description, :price
+  attr_accessor :description, :price_before_tax
   attr_reader :tax_rate_calculator
   
-  def initialize(description, price, tax_decorators=[])
+  def initialize(description, price, tax_decorators=[], tax_rate_calculator_factory=TaxRateCalculatorFactory)
     @description = description
-    @price = price
-    @tax_rate_calculator = TaxRateCalculatorFactory.load_tax_rate_calculator(tax_decorators)
+    @price_before_tax = price
+    @tax_rate_calculator = tax_rate_calculator_factory.load_tax_rate_calculator(tax_decorators)
+  end
+  
+  def tax
+    @price_before_tax * @tax_rate_calculator.calculate
+  end
+  
+  def price
+    @price_before_tax + tax
+  end
+  
+  def describe_yourself
+    description.lstrip.rstrip + " : " + sprintf("%.2f", price)
   end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -3,7 +3,36 @@ require 'spec_helper'
 describe Product do
   describe "product attributes and methods" do
     it "should have a description, price, tax_rate_calculator, and tax_decorators" do
-      Product.new(nil, nil).should respond_to(:description, :price, :tax_rate_calculator)
+      Product.new(nil, nil).should respond_to(:description, :price_before_tax, :tax_rate_calculator, :price, :tax)
+    end
+  end
+  
+  describe 'initializer' do
+    it 'should load a tax_rate_calculator' do
+      Product.new(nil, nil).tax_rate_calculator.should be_an_instance_of(TaxRateCalculator)
+    end
+  end
+  
+  describe 'describe_yourself' do
+    it 'should return a composed of the name and price of the product' do
+      TaxRateCalculatorFactory.stub!(:load_tax_rate_calculator).and_return(TaxRateCalculator.new(nil, 0))
+      Product.new("imported bottle of medicine", 100.00, [], TaxRateCalculatorFactory).describe_yourself.should == "imported bottle of medicine : 100.00"
+    end
+  end
+  
+  describe 'price' do
+    it 'should return the price of the product with tax' do
+      TaxRateCalculatorFactory.stub!(:load_tax_rate_calculator).and_return(TaxRateCalculator.new(nil, 0.1))
+      product = Product.new("imported bottle of medicine", 100.00, [], TaxRateCalculatorFactory)
+      product.price.should == 110.00
+    end
+  end
+  
+  describe 'tax' do
+    it 'should return the tax that is going to be charged on this product' do
+      TaxRateCalculatorFactory.stub!(:load_tax_rate_calculator).and_return(TaxRateCalculator.new(nil, 0.1))
+      product = Product.new("imported bottle of medicine", 100.00, [], TaxRateCalculatorFactory)
+      product.tax.should == 10.00
     end
   end
 end
